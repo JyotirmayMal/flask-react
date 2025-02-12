bdd94d65c7cf7f1d811f9710208c1055c951c78f
JyotirmayMal <jyotirmaymal02@gmail.com>
2025-02-12 12:08:53 +0530

    first commit

---
+++ calculator/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/main', methods=['GET', 'POST'])
+def calculator():
+    result = None
+    if request.method == 'POST':
+        num1 = request.form.get('num1')
+        num2 = request.form.get('num2')
+        operation = request.form.get('operation')
+
+        num1, num2 = float(num1), float(num2)
+
+        if operation == 'add':
+            result = num1 + num2
+        elif operation == 'subtract':
+            result = num1 - num2
+        elif operation == 'multiply':
+            result = num1 * num2
+        elif operation == 'divide':
+            result = num1 / num2 if num2 != 0 else "Cannot divide by zero"
+        else:
+            result = "Invalid Operation"
+
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
